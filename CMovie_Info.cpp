bfbf5ddf4e885d73b28c5c4b49dbbba3a5761444
YewonLim <ga06033@o365.yonsei.ac.kr>
2021-04-30 04:20:31 +0900

    complete movie info

---
+++ CMovie_Info.cpp
@@ -0,0 +1,15 @@
+#include "CMovie_Info.h"
+
+CMovie_Info::CMovie_Info(string name, int num): m_moviename(name), m_schedule_num(num){
+    m_movie_schedule = new schedule[m_schedule_num];
+}
+
+const string CMovie_Info::get_name() const{
+    return this->m_moviename;
+}
+
+schedule* CMovie_Info::get_movie_schedule() const{
+    return this->m_movie_schedule;
+}
+
+~CMovie_Info() {delete[] this->m_movie_schedule;}
--- CMovie_Info.h
+++ CMovie_Info.h
@@ -1,6 +1,7 @@
 #ifndef __CMOVIEINFO__
 #define __CMOVIEINFO__
 
+#define MAX_NUM_OF_
 #include <string>
 
 using namespace std;
@@ -9,15 +10,17 @@ enum schedule {MORNIG_1, MORNIG_2, MORNIG_3,
                AFTERNOON_1, AFTERNOON_2, AFTERNOON_3,
                EVENING_1, EVENING_2, EVENING_3};
 
-
-
 class CMovie_Info{
 private:
-    string m_moviename;
-    schedule m_movie_schedule[];
+    const string m_moviename;
+    schedule* m_movie_schedule;
+    const int m_schedule_num;
 
 public:
-    
+    CMovie_Info(string name, int num);
+    const string get_movie_name() const;
+    schedule* get_movie_schedule() const;
+    ~CMovie_Info();
 };
 
 #endif
