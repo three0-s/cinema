2747c2bb60eb4da006863d07f14b6031050889da
YewonLim <ga06033@o365.yonsei.ac.kr>
2021-05-02 01:40:48 +0900

    modify CSeat  -fixed few errors

--- CSeat.cpp
+++ CSeat.cpp
@@ -1,7 +1,8 @@
 #include "CSeat.h"
 
-CSeat::CSeat(){}
-
+void CSeat::init_seatnum(){
+	m_seatnum = 0;
+}
 CSeat::CSeat(std::string name, std::string phonenumber, bool gender, std::string movie_name, int schedule_num) :
 	CPerson_Info(name, phonenumber, gender), CMovie_Info(movie_name, schedule_num) {};
 
@@ -9,3 +10,4 @@ void CSeat::addnum() {}
 
 int CSeat::m_seatnum = 0;
 
+void CSeat::addnum() { this->m_seatnum++; }
--- CSeat.h
+++ CSeat.h
@@ -6,16 +6,14 @@
 #include"CPerson_Info.h"
 #include"CMovie_Info.h"
 
-class CSeat : public CPerson_Info, public CMovie_Info {
+class CSeat : public CPerson_Info, public CMovie_Info{
 private:
 	static int m_seatnum;
 
 public:
 	CSeat(std::string name, std::string phonenumber, bool gender, std::string movie_name, int schedule_num);
-	CSeat();
-	static void addnum();
+	void addnum();
+	void init_seatnum();
 };
-
-
 
 #endif
